ef4b502a5e030da9e0293ed269fcf4d3ff13bda7
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2022-12-28 11:46:13 -0700

    forgot to add files

---
+++ src/components/RequestInfo.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function RequestInfo () {
+  return (
+    <div>
+      <p>REQUEST INFO PAGE.</p>
+    </div>
+  )
+
+}
+
+export default RequestInfo
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function SignUp () {
+  return (
+    <div>
+      <p>SIGN UP PAGE.</p>
+    </div>
+  )
+
+}
+
+export default SignUp
