460661a4c9c56d87ec41645bc744df2de363b5b7
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2022-12-28 11:45:08 -0700

    added basic swapping between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,33 @@
 import { useState } from 'react'
+import Home from './components/Home'
+import Gifts from './components/Gifts'
+import RequestInfo from './components/RequestInfo'
+import SignUp from './components/SignUp'
+import HowItWorks from './components/HowItWorks'
 import './App.css'
 
 function App() {
 
+  const [page, setPage] = useState('Home');
+
+  const navClick = (e) => {
+    e.preventDefault();
+    setPage(e.target.innerText);
+  }
+
   return (
 
     <div className="App">
-    <img src='http://natalieselfshelf.com/images/elf%20shelf%20logo%2089.png?crc=3903930728' className='Logo'/>
-      <div className='navBar'><p>Home | How It Works | Gifts | Request Information | Sign Up Today</p></div>
-        <p>Elf Shelf® Holiday Shop is a temporary, in-school gift shop program designed for Elementary and Middle School PTA/PTO/Parent groups that want to bring a free, fun and educational service project to their school.</p>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/ETso1d7SqrU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      <h3>Gifts are tested and kid-priced for Mom, Dad, Grandma, Grandpa and friends.{<br/>}
-Everything is sent on consignment, so there is no prepayment required – we even pay the shipping!{<br/>}
-We make it easy for you to have a holiday shop at your school.</h3>
-        <table>
-          <tbody>
-            <tr>
-              <td>100% Consignment</td>
-              <td>Free Gift Wrap Supplies</td>
-            </tr>
-            <tr>
-              <td>Most Gifts $7 and Less</td>
-              <td>Free Promotional Kit</td>
-            </tr>
-            <tr>
-              <td>No Inventory Required</td>
-              <td>Price-Programmed Cash Register</td>
-            </tr>
-          </tbody>
-        </table>
-    <p className='Info'>For Questions and Information{<br/>}
-Call 1-877-384-9653{<br/>}
-Email info@elfshelfholidayshop.com</p>
+      <img src='http://natalieselfshelf.com/images/elf%20shelf%20logo%2089.png?crc=3903930728' className='Logo'/>
+      <div className='navBar'><p><button onClick={navClick}>Home</button> | <button onClick={navClick}>How It Works</button> | <button onClick={navClick}>Gifts</button> | <button onClick={navClick}>Request Information</button> | <button onClick={navClick}>Sign Up Today</button></p></div>
+        {page === 'Home' ? <Home /> : null}
+        {page === 'How It Works' ? <HowItWorks /> : null}
+        {page === 'Gifts' ? <Gifts /> : null}
+        {page === 'Request Information' ? <RequestInfo /> : null}
+        {page === 'Sign Up Today' ? <SignUp /> : null}
+      <p className='Info'>For Questions and Information{<br/>}
+      Call 1-877-384-9653{<br/>}
+      Email info@elfshelfholidayshop.com</p>
     </div>
   )
 }
