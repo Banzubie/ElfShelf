b085641780c15ba52e22f50d83ee3e1b9a682ce8
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2023-01-02 13:27:39 -0700

    changed registered trademark and added 'The' to all instances of 'Elf Shelf'

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,11 @@ import { useState } from 'react'
 function Home () {
   return (
     <div>
-      <p>Elf Shelf® Holiday Shop is a temporary, in-school gift shop program designed for Elementary and Middle School PTA/PTO/Parent groups that want to bring a free, fun and educational service project to their school.</p>
+      <p>The Elf Shelf<span class="sup">&reg;</span> Holiday Shop is a temporary, in-school gift shop program designed for Elementary and Middle School PTA/PTO/Parent groups that want to bring a free, fun and educational service project to their school.</p>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/ETso1d7SqrU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-    <h3>Gifts are tested and kid-priced for Mom, Dad, Grandma, Grandpa and friends.{<br/>}
+    <p>Gifts are tested and kid-priced for Mom, Dad, Grandma, Grandpa and friends.{<br/>}
   Everything is sent on consignment, so there is no prepayment required – we even pay the shipping!{<br/>}
-  We make it easy for you to have a holiday shop at your school.</h3>
+  We make it easy for you to have a holiday shop at your school.</p>
       <table>
         <tbody>
           <tr>
