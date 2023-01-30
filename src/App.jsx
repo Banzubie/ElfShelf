42a1a91569a9d4c7a86f013d87906721c4bdf783
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2023-01-29 19:05:29 -0700

    added carousel and margin to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,13 @@ function App() {
         <img src='src/assets/ElfPointLeft.jpeg' style={{position: 'relative' , top: '30px',height: '153px'}}/>
       </div>
       <div className='navBar'><p><button onClick={navClick}>Home</button> | <button onClick={navClick}>How It Works</button> | <button onClick={navClick}>Gifts</button> | <button onClick={navClick}>Request Information</button> | <button onClick={navClick}>Sign Up Today</button></p></div>
+      <div style={{margin: '5%'}}>
         {page === 'Home' ? <Home /> : null}
         {page === 'How It Works' ? <HowItWorks /> : null}
         {page === 'Gifts' ? <Gifts /> : null}
         {page === 'Request Information' ? <RequestInfo requested={requested} setRequested={setRequested}/> : null}
         {page === 'Sign Up Today' ? <SignUp requested={requested} setRequested={setRequested}/> : null}
+      </div>
       <p className='Info'>For Questions and Information{<br/>}
       Call 1-877-384-9653{<br/>}
       Email info@elfshelfholidayshop.com</p>
--- src/components/Gifts.jsx
+++ src/components/Gifts.jsx
@@ -1,19 +1,62 @@
-import { useState } from 'react'
-import GiftsModal from './GiftComponents/GiftsModal.jsx'
+import { useState, useEffect } from 'react'
 
 function Gifts () {
   const [showModal, setShowModal] = useState(false);
+  const [index, setIndex] = useState(0);
+  const pictures = [
+    'src/assets/giftImg/WOOD PUZZLE.png',
+    'src/assets/giftImg/USB NIGHT LIGHT.png',
+    'src/assets/giftImg/ROTATING STAR LIGHT PROJECTION LAMP.png',
+    'src/assets/giftImg/RETRO MOTORCYCLE ALARM CLOCK.png',
+    'src/assets/giftImg/OWLS BROACH.png',
+    'src/assets/giftImg/M0M HEART RING.png',
+    'src/assets/giftImg/LIGHT UP TOP.png',
+    'src/assets/giftImg/LADIES WATCH.png',
+    'src/assets/giftImg/FOUNTAIN PEN AND BALL POINT PEN GIFT SET.png',
+    'src/assets/giftImg/FINGERTIP MONSTER GYRO.png',
+    'src/assets/giftImg/FAMILY PENS.png',
+    'src/assets/giftImg/FABRIC HOLIDAY WREATH.png',
+    'src/assets/giftImg/DINOSAUR GEL PENS.png',
+    'src/assets/giftImg/CRYSTAL MOM HEART NECKLACE.png',
+    'src/assets/giftImg/CELL PHONE PURSE.png',
+    'src/assets/giftImg/CAT PENDANT BRACELET.png'
+  ];
+
+  const nextPict = () => {
+    if (index === pictures.length - 1) {
+      return setIndex(0);
+    }
+    return setIndex(index + 1);
+  }
+
+  const prevPict = () => {
+    if (index === 0) {
+      return setIndex(pictures.length - 1);
+    }
+    return setIndex(index - 1);
+  }
+  useEffect(() => {
+    const interval = setInterval(nextPict, 3000)
+
+    return () => clearInterval(interval)
+  })
   return (
     <div>
-      <img src='src/assets/ElfGifts.png' className='GiftsElfPic'></img>
+      <div className='carouselBox'>
+        <button className='carouselPrev' onClick={prevPict}>{'<'}</button>
+        <div className='carousel'>
+        {pictures.map((pict, idx) => {
+          return <img className='carouselImg' src={pict} style={{transform: `translate(-${index * 100}%)`}} key={idx}></img>
+        })}
+        </div>
+        <button className='carouselNext' onClick={nextPict}>{'>'}</button>
+        <img src='src/assets/ElfGifts.png' className='GiftsElfPic'></img>
+      </div>
       <p>
       The Elf Shelf<span className="sup">&reg;</span> Holiday Shop gifts are selected by a team of purchasers during a two-week trip to China every January. After a variety of gifts are selected, SchoolPals (the parent company of The Elf Shelf<span className="sup">&reg;</span> Holiday Shop) participates in a process to discuss which gifts should be chosen.  The selected gifts then go through an independent testing process. All of The Elf Shelf<span className="sup">&reg;</span> Holiday Shop gifts must pass quality control and the Federal CPSIA testing requirements to be included in our gift line. Test results are available upon request.
 
 100% Gift Guarantee – If at any time a gift is damaged, simply contact us and a replacement will be shipped directly to the purchaser free of charge.  No questions asked!
       </p>
-      <br/>
-      <button onClick={() => (setShowModal(true))}>See some of the gifts!</button>
-      {showModal ? <GiftsModal setShowModal={setShowModal} /> : null}
     </div>
   )
 }
