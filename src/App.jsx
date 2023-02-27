4442293c761fe904881dde448d56ff538c57b2fb
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2023-02-26 19:58:05 -0700

    small updates to wording

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Home from './components/Home'
 import Gifts from './components/Gifts'
 import RequestInfo from './components/RequestInfo'
@@ -21,26 +21,26 @@ function App() {
   return (
     <div className="App">
       <div className='logo'>
-        <img src='src/assets/elfshelflogo.jpg' alt='The Elf Shelf logo'/>
-        <img src='src/assets/ElfPointLeft.jpeg' style={{position: 'relative' , top: '30px',height: '153px'}} alt='elf pointing with index finger to the logo'/>
+        <img src='src/assets/elfshelflogo.jpg' alt='The Elf Shelf logo' />
+        <img src='src/assets/ElfPointLeft.jpeg' style={{ position: 'relative', top: '30px', height: '153px' }} alt='elf pointing with index finger to the logo' />
       </div>
       <div className='navBar'><p><button onClick={navClick}>Home</button><button onClick={navClick}>How It Works</button><button onClick={navClick}>Gifts</button><button onClick={navClick}>Request Information</button><button onClick={navClick}>Sign Up Today</button></p></div>
-      <div style={{margin: '-1em 5em'}}>
+      <div style={{ margin: '-1em 5em' }}>
         {page === 'Home' ? <Home /> : null}
         {page === 'How It Works' ? <HowItWorks /> : null}
         {page === 'Gifts' ? <Gifts /> : null}
-        {page === 'Request Information' ? <RequestInfo requested={requested} setRequested={setRequested}/> : null}
-        {page === 'Sign Up Today' ? <SignUp requested={requested} setRequested={setRequested}/> : null}
+        {page === 'Request Information' ? <RequestInfo requested={requested} setRequested={setRequested} /> : null}
+        {page === 'Sign Up Today' ? <SignUp requested={requested} setRequested={setRequested} /> : null}
       </div>
-      <p className='Info'><span style={{fontSize: '10px', fontStyle: 'italic'}}>The Elf Shelf Holiday Shop is brought to you by Schoolpals, LLC</span>{<br/>}
-      For Questions and Information{<br/>}
-      Call <a href='tel:18773849653'>1-877-384-9653</a>{<br/>}
-      Email <a href='mailto:info@elfshelfholidayshop.com'>info@elfshelfholidayshop.com</a>{<br/>}
-      <span style={{fontSize: '10px'}}>Website by Brian Vose <a href="https://github.com/Banzubie" target="_blank">
+      <p className='Info'><span style={{ fontSize: '10px', fontStyle: 'italic' }}>The Elf Shelf<span className="sup">&reg;</span> Holiday Shop is brought to you by SchoolPals, LLC</span>{<br />}
+        For Questions and Information{<br />}
+        Call <a href='tel:18773849653'>1-877-384-9653</a>{<br />}
+        Email <a href='mailto:info@elfshelfholidayshop.com'>info@elfshelfholidayshop.com</a>{<br />}
+        <span style={{ fontSize: '10px' }}>Website by Brian Vose <a href="https://github.com/Banzubie" target="_blank">
           Github
         </a> <a href="https://www.linkedin.com/in/brvose/" target="_blank">
-          LinkedIn
-        </a></span></p>
+            LinkedIn
+          </a></span></p>
     </div>
   )
 }
--- src/components/Gifts.jsx
+++ src/components/Gifts.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-function Gifts () {
+function Gifts() {
   const [showModal, setShowModal] = useState(false);
   const [index, setIndex] = useState(0);
   const pictures = [
@@ -45,18 +45,18 @@ function Gifts () {
       <div className='carouselBox'>
         <button className='carouselPrev' onClick={prevPict}>{'<'}</button>
         <div className='carousel'>
-        {pictures.map((pict, idx) => {
-          return <img className='carouselImg' src={pict} style={{transform: `translate(-${index * 100}%)`}} key={idx}></img>
-        })}
+          {pictures.map((pict, idx) => {
+            return <img className='carouselImg' src={pict} style={{ transform: `translate(-${index * 100}%)` }} key={idx}></img>
+          })}
         </div>
         <button className='carouselNext' onClick={nextPict}>{'>'}</button>
       </div>
       <p>
-      The Elf Shelf<span className="sup">&reg;</span> Holiday Shop gifts are selected by a team of purchasers during a two-week trip to China every January. After a variety of gifts are selected, SchoolPals (the parent company of The Elf Shelf<span className="sup">&reg;</span> Holiday Shop) participates in a process to discuss which gifts should be chosen.  The selected gifts then go through an independent testing process. All of The Elf Shelf<span className="sup">&reg;</span> Holiday Shop gifts must pass quality control and the Federal CPSIA testing requirements to be included in our gift line. Test results are available upon request.
+        The Elf Shelf<span className="sup">&reg;</span> Holiday Shop gifts are selected by a team of purchasers during a two-week merchandising trip every January. After a variety of gifts are selected, SchoolPals (an authorized rep of The Elf Shelf<span className="sup">&reg;</span> Holiday Shop) participates in a process to discuss which gifts should be chosen.  The selected gifts then go through an independent testing process. All of The Elf Shelf<span className="sup">&reg;</span> Holiday Shop gifts must pass quality control and the Federal CPSIA testing requirements to be included in our gift line. Test results are available upon request.
 
-100% Gift Guarantee – If at any time a gift is damaged, simply contact us and a replacement will be shipped directly to the purchaser free of charge.  No questions asked!
+        100% Gift Guarantee – If at any time a gift is damaged, simply contact us and a replacement will be shipped directly to the purchaser free of charge.  No questions asked!
       </p>
-        <img src='src/assets/ElfGifts.png' className='GiftsElfPic'></img>
+      <img src='src/assets/ElfGifts.png' className='GiftsElfPic'></img>
     </div>
   )
 }
